775bf8c33bc93fc055deec57e475790b287cfc4a
Yammani Shashwanth Kumar <117337862+shashwanthkumar@users.noreply.github.com>
2024-10-24 19:18:56 +0530

    Add files via upload

---
+++ js_2.js
@@ -0,0 +1,60 @@
+
+
+let hov=document.getElementById("hov").style.marginLeft="50px";
+
+let a=document.getElementById("a1").style.textDecoration="none";
+
+function cl(){
+    let p=document.getElementById("p1")
+    p.innerHTML="<b style='color:red;font-size:30px'>Excellent view of moon in the night at mountain</b>"
+}
+function cll(){
+    let p=document.getElementById("p1")
+    p.innerHTML=""
+}
+
+
+function opacity1(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='lake-blue-moonlight-moon-wallpaper-preview.jpg' style='opacity:0'>"
+}
+function opacity2(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='lake-blue-moonlight-moon-wallpaper-preview.jpg' style='opacity:0.1'>"
+}
+function opacity3(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='lake-blue-moonlight-moon-wallpaper-preview.jpg' style='opacity:0.2'>"
+}
+function opacity4(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='lake-blue-moonlight-moon-wallpaper-preview.jpg' style='opacity:0.3'>"
+}
+function opacity5(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='lake-blue-moonlight-moon-wallpaper-preview.jpg' style='opacity:0.4'>"
+}
+function opacity6(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='lake-blue-moonlight-moon-wallpaper-preview.jpg' style='opacity:0.5'>"
+}
+function opacity7(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='lake-blue-moonlight-moon-wallpaper-preview.jpg' style='opacity:0.6'>"
+}
+function opacity8(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='lake-blue-moonlight-moon-wallpaper-preview.jpg' style='opacity:0.7'>"
+}
+function opacity9(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='lake-blue-moonlight-moon-wallpaper-preview.jpg' style='opacity:0.8'>"
+}
+function opacity10(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='lake-blue-moonlight-moon-wallpaper-preview.jpg' style='opacity:0.9'>"
+}
+function opacity11(){
+    let img=document.getElementsByClassName("size")
+    img[0].innerHTML="<img src='lake-blue-moonlight-moon-wallpaper-preview.jpg' style='opacity:1'>"
+}
